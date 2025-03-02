3228cd23dea5bca505b629f60d9f32fe0970bb0c
MrTup <lookchunkiu@gmail.com>
2025-03-02 20:12:22 +0000

    Video Stream

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import cv2
+###from deepface import DeepFace
 
-img = cv2.imread("Messi1.jpg")
-cv2.imshow("Img", img)
-cv2.waitKey(0)
+cap = cv2.VideoCapture(0)
+
+
+counter = 0
+face_match = False
+
+while True:
+    ret, frame = cap.read()
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
